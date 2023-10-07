616e298af31df0f779eb636d1af4c4e6b47a987d
Ming Li <minglii@nexuscml00.umiacs.umd.edu>
2023-10-07 12:33:06 -0400

    init

--- 20231006/test_lm_13b_1.sh
+++ 20231006/test_lm_13b_1.sh
@@ -10,10 +10,31 @@ export HF_HOME=/export/jchen169/Ming/cache
 export TRANSFORMERS_CACHE=/export/jchen169/Ming/cache
 export CUDA_VISIBLE_DEVICES=4,5
 
+# Run ARC
+python main.py \
+    --model hf-causal-experimental \
+    --model_args pretrained=$MODEL_PATH,use_accelerate=True \
+    --tasks arc_challenge \
+    --batch_size 1 \
+    --output_path results/$MODEL_NAME/ARC.json \
+    --no_cache \
+    --device cuda \
+    --num_fewshot 25
+
+# Run TruthfulQA
+python main.py \
+    --model hf-causal-experimental \
+    --model_args pretrained=$MODEL_PATH,use_accelerate=True \
+    --tasks truthfulqa_mc \
+    --batch_size 1 \
+    --output_path results/$MODEL_NAME/TruthfulQA.json \
+    --no_cache \
+    --device cuda
+
 # Run HellaSwag
 python main.py \
     --model hf-causal-experimental \
-    --model_args pretrained=$MODEL_PATH \
+    --model_args pretrained=$MODEL_PATH,use_accelerate=True \
     --tasks hellaswag \
     --batch_size 1 \
     --output_path results/$MODEL_NAME/HellaSwag.json \
